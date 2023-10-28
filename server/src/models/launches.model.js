364c869eeed4282d9b873aac1f4982f44de03283
trucvy200200 <toan9400@gmail.com>
2023-10-28 18:53:20 +0700

    first commit

---
+++ server/src/models/launches.model.js
@@ -0,0 +1,50 @@
+const launches = new Map();
+
+let latestFlightNumber = 100;
+
+const launch = {
+    flightNumber: 100,
+    mission: 'Kepler Exploration X',
+    rocket: 'Explorer IS1',
+    launchDate: new Date('December 27, 2030'),
+    destination: 'Kepler-442 b',
+    customer: ['ZTM', 'NASA'],
+    upcoming: true,
+    success: true
+}
+
+launches.set(launch.flightNumber, launch);
+
+function existsLaunchWithId(launchId) {
+    return launches.has(launchId);
+}
+function getAllLaunches() {
+    console.log(launches);
+    return Array.from(launches.values());
+}
+
+function addNewLaunch(launch) {
+    latestFlightNumber++;
+    launches.set(
+        latestFlightNumber,
+        Object.assign(launch, {
+            success: true,
+            upcoming: true,
+            customer: ['Zero To Mastery', 'NASA'],
+            flightNumber: latestFlightNumber
+        })
+    );
+}
+
+function abortLaunchById(launchId) {
+    const aborted = launches.get(launchId);
+    aborted.upcoming = false;
+    aborted.success = false;
+    return aborted
+}
+module.exports = {
+    existsLaunchWithId,
+    getAllLaunches,
+    addNewLaunch,
+    abortLaunchById
+}
